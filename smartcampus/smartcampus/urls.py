f0d741365242f56b973543b25c38a9e687817463
kxue4 <kxue4@stevens.edu>
2018-10-05 18:17:35 -0400

    Updated Location, Report, Feedback UI

--- smartcampus/smartcampus/urls.py
+++ smartcampus/smartcampus/urls.py
@@ -19,4 +19,8 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls')),
+    path('home/', include('core.urls')),
+    path('select_location/', include('core.urls')),
+    path('campus_report/', include('core.urls')),
+    path('feedback/', include('core.urls'))
 ]
