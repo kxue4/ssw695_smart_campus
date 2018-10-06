47b8cfb9fb59d349124823c87cfc7ab8b35faa0b
vporta <vporta7@gmail.com>
2018-10-05 22:09:48 -0400

    mysql db setup. New models added. Django mysql configured

--- smartcampus/core/models.py
+++ smartcampus/core/models.py
@@ -20,8 +20,34 @@ class Location(models.Model):
 class Temperature(models.Model):
     id = models.IntegerField(primary_key=True)
     temperature = models.DecimalField(max_digits=10, decimal_places=2)
-    location = models.ForeignKey(Location, on_delete=models.CASCADE)
+    location_id = models.ForeignKey(Location, on_delete=models.CASCADE)
 
 
+class Pressure(models.Model):
+    id = models.IntegerField(primary_key=True)
+    pressure_value = models.DecimalField(max_digits=10, decimal_places=2)
+    location_id = models.ForeignKey(Location, on_delete=models.CASCADE)
 
 
+class Humidity(models.Model):
+    id = models.IntegerField(primary_key=True)
+    humidity_level = models.DecimalField(max_digits=10, decimal_places=2)
+    location_id = models.ForeignKey(Location, on_delete=models.CASCADE)
+
+
+class Decibel(models.Model):
+    id = models.IntegerField(primary_key=True)
+    decibel_level = models.DecimalField(max_digits=10, decimal_places=2)
+    location_id = models.ForeignKey(Location, on_delete=models.CASCADE)
+
+
+class AirQuality(models.Model):
+    id = models.IntegerField(primary_key=True)
+    air_quality_level = models.IntegerField()
+    location_id = models.ForeignKey(Location, on_delete=models.CASCADE)
+
+
+class CampusBuilding(models.Model):
+    id = models.IntegerField(primary_key=True)
+    building_name = models.CharField(max_length=120)
+    location_id = models.CharField(max_length=120)
--- smartcampus/smartcampus/settings.py
+++ smartcampus/smartcampus/settings.py
@@ -74,13 +74,23 @@ WSGI_APPLICATION = 'smartcampus.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.environ.get('DATABASE_NAME', ''),
+        'USER': os.environ.get('DATABASE_USER', ''),
+        'PASSWORD': os.environ.get('DATABASE_PASSWORD', ''),
+        'HOST': os.environ.get('DATABASE_HOST', ''),
+        'PORT': os.environ.get('DATABASE_PORT', ''),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
