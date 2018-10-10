8e04af799d2b8fd263693d8af899fe2b53856d91
xli119 <xli119@stevens.edu>
2018-10-09 20:00:02 -0400

    add switching maps function

---
+++ smartcampus/core/static/scripts/airqualitymap.js
@@ -0,0 +1,36 @@
+// map center
+var myLatlng = new google.maps.LatLng(40.742770, -74.026230);
+// map options,
+var myOptions = {
+    zoom: 16,
+    center: myLatlng
+};
+// standard map
+map = new google.maps.Map(document.getElementById("air_quality_map"), myOptions);
+// heatmap layer
+heatmap = new HeatmapOverlay(map, 
+    {
+    // radius should be small ONLY if scaleRadius is true (or small radius is intended)
+    "radius": 25,
+    "maxOpacity": 1, 
+    // scales the radius based on map zoom
+    "scaleRadius": false, 
+    // if set to false the heatmap uses the global maximum for colorization
+    // if activated: uses the data maximum within the current map boundaries 
+    //   (there will always be a red spot with useLocalExtremas true)
+    "useLocalExtrema": true,
+    // which field name in your data represents the latitude - default "lat"
+    latField: 'lat',
+    // which field name in your data represents the longitude - default "lng"
+    lngField: 'lng',
+    // which field name in your data represents the data value - default "value"
+    valueField: 'count'
+    }
+);
+
+var testData = {
+    max: 3,
+    data: [{lat: 40.742770, lng:-74.026230, count: 13},{lat: 40.745459, lng:-74.022585, count: 5}, {lat: 40.743427, lng:-74.0272672, count: 4}]
+};
+
+heatmap.setData(testData);
---
+++ smartcampus/core/static/scripts/switchmap.js
@@ -0,0 +1,19 @@
+window.onload = function(){
+    var btnGroup = document.getElementsByClassName("btn-group")[0];
+    var myButton = btnGroup.getElementsByTagName("button");
+    var myDiv = document.getElementsByClassName("map-canvas");
+
+    for(var i = 0; i<myButton.length;i++){
+        myButton[i].index = i;
+        myButton[i].onclick = function(){
+            for(var i = 0; i < myButton.length; i++){
+                myButton[i].className="btn btn-default";
+                myDiv[i].style.display="none";
+            }
+            this.className = "btn btn-primary";
+            myDiv[this.index].style.display = "block";
+        }
+      }
+}
+
+
