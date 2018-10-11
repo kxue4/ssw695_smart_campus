41771328aa1ffc6cdd8e3817d7b5b1ac9da2d826
xli119 <xli119@stevens.edu>
2018-10-10 23:34:22 -0400

    display with data from database

--- smartcampus/core/static/scripts/temperaturemap.js
+++ smartcampus/core/static/scripts/temperaturemap.js
@@ -11,26 +11,28 @@ map = new google.maps.Map(document.getElementById("temperature_map"), myOptions)
 heatmap = new HeatmapOverlay(map, 
     {
     // radius should be small ONLY if scaleRadius is true (or small radius is intended)
-    "radius": 25,
+    "radius": 30,
     "maxOpacity": 1, 
     // scales the radius based on map zoom
     "scaleRadius": false, 
     // if set to false the heatmap uses the global maximum for colorization
     // if activated: uses the data maximum within the current map boundaries 
     //   (there will always be a red spot with useLocalExtremas true)
-    "useLocalExtrema": true,
+    "useLocalExtrema": false,
     // which field name in your data represents the latitude - default "lat"
     latField: 'lat',
     // which field name in your data represents the longitude - default "lng"
     lngField: 'lng',
     // which field name in your data represents the data value - default "value"
-    valueField: 'count'
+    valueField: 'value'
     }
 );
 
+var tempValue = parseInt(document.getElementById("temp_value").innerHTML);
 var testData = {
-    max: 3,
-    data: [{lat: 40.742770, lng:-74.026230, count: 3},{lat: 40.745459, lng:-74.022585, count: 5}]
+    max: 60,
+    data: [{lat: 40.743427, lng:-74.0272672, value: tempValue}]
 };
+//{lat: 40.742770, lng:-74.026230, count: 3},{lat: 40.745459, lng:-74.022585, count: 5}
 
 heatmap.setData(testData);
