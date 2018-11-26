f0ab1f952ff4ed6bd312fd94454ffc1109a42e32
vporta <vporta7@gmail.com>
2018-11-26 14:45:06 -0500

    added shell script

--- activate.sh
+++ activate.sh
@@ -1,7 +1,8 @@
-DATABASE_NAME=readings01
-DATABASE_USER=stevens
-DATABASE_PASSWORD=Stevens01
-DATABASE_HOST=sc01reading.cowctspza6qy.us-west-2.rds.amazonaws.com
-DATABASE_PORT=3306
-SENDGRID_API_KEY=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
+#!/usr/bin/env bash
+export DATABASE_NAME="readings01"
+export DATABASE_USER="stevens"
+export DATABASE_PASSWORD="Stevens01"
+export DATABASE_HOST="sc01reading.cowctspza6qy.us-west-2.rds.amazonaws.com"
+export DATABASE_PORT=3306
+export SENDGRID_API_KEY="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"
 
