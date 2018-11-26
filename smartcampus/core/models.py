5a5e9550e35e73c90588fcf63655b6bb8b31f829
vporta <vporta7@gmail.com>
2018-11-26 14:43:57 -0500

    running unit tests on models without errors

--- smartcampus/core/models.py
+++ smartcampus/core/models.py
@@ -1,71 +1,6 @@
 from django.db import models
 
 
-class AuthGroup(models.Model):
-    name = models.CharField(unique=True, max_length=80)
-
-    class Meta:
-        db_table = 'auth_group'
-        managed = False
-
-
-class AuthGroupPermissions(models.Model):
-    group = models.ForeignKey(AuthGroup, on_delete=models.CASCADE)
-    permission = models.ForeignKey('AuthPermission', on_delete=models.CASCADE)
-
-    class Meta:
-        db_table = 'auth_group_permissions'
-        managed = False
-        unique_together = (('group', 'permission'),)
-
-
-class AuthPermission(models.Model):
-    name = models.CharField(max_length=255)
-    content_type = models.ForeignKey('DjangoContentType', on_delete=models.CASCADE)
-    codename = models.CharField(max_length=100)
-
-    class Meta:
-        db_table = 'auth_permission'
-        unique_together = (('content_type', 'codename'),)
-
-
-class AuthUser(models.Model):
-    password = models.CharField(max_length=128)
-    last_login = models.DateTimeField(blank=True, null=True)
-    is_superuser = models.IntegerField()
-    username = models.CharField(unique=True, max_length=150)
-    first_name = models.CharField(max_length=30)
-    last_name = models.CharField(max_length=150)
-    email = models.CharField(max_length=254)
-    is_staff = models.IntegerField()
-    is_active = models.IntegerField()
-    date_joined = models.DateTimeField()
-
-    class Meta:
-        db_table = 'auth_user'
-        managed = False
-
-
-class AuthUserGroups(models.Model):
-    user = models.ForeignKey(AuthUser, on_delete=models.CASCADE)
-    group = models.ForeignKey(AuthGroup, on_delete=models.CASCADE)
-
-    class Meta:
-        db_table = 'auth_user_groups'
-        managed = False
-        unique_together = (('user', 'group'),)
-
-
-class AuthUserUserPermissions(models.Model):
-    user = models.ForeignKey(AuthUser, on_delete=models.CASCADE)
-    permission = models.ForeignKey(AuthPermission, on_delete=models.CASCADE)
-
-    class Meta:
-        db_table = 'auth_user_user_permissions'
-        managed = False
-        unique_together = (('user', 'permission'),)
-
-
 class Airquality(models.Model):
     id = models.IntegerField(primary_key=True)
     air_quality_level = models.IntegerField()
@@ -179,46 +114,6 @@ class User(models.Model):
         db_table = 'core_user'
 
 
-class DjangoAdminLog(models.Model):
-    action_time = models.DateTimeField()
-    object_id = models.TextField(blank=True, null=True)
-    object_repr = models.CharField(max_length=200)
-    action_flag = models.PositiveSmallIntegerField()
-    change_message = models.TextField()
-    content_type = models.ForeignKey('DjangoContentType', on_delete=models.CASCADE, blank=True, null=True)
-    user = models.ForeignKey(AuthUser, on_delete=models.CASCADE)
-
-    class Meta:
-        db_table = 'django_admin_log'
-
-
-class DjangoContentType(models.Model):
-    app_label = models.CharField(max_length=100)
-    model = models.CharField(max_length=100)
-
-    class Meta:
-        db_table = 'django_content_type'
-        unique_together = (('app_label', 'model'),)
-
-
-class DjangoMigrations(models.Model):
-    app = models.CharField(max_length=255)
-    name = models.CharField(max_length=255)
-    applied = models.DateTimeField()
-
-    class Meta:
-        db_table = 'django_migrations'
-
-
-class DjangoSession(models.Model):
-    session_key = models.CharField(primary_key=True, max_length=40)
-    session_data = models.TextField()
-    expire_date = models.DateTimeField()
-
-    class Meta:
-        db_table = 'django_session'
-
-
 class Data(models.Model):
     # id = models.IntegerField(primary_key=True)
     mac = models.CharField(max_length=17, blank=True, null=True)
@@ -233,22 +128,6 @@ class Data(models.Model):
     class Meta:
         # managed = False
         db_table = 'data'
-
-
-class Zerodata(models.Model):
-    # id = models.IntegerField(primary_key=True)
-    mac = models.CharField(max_length=17, blank=True, null=True)
-    temp = models.FloatField(blank=True, null=True)
-    pres = models.FloatField(blank=True, null=True)
-    hum = models.FloatField(blank=True, null=True)
-    gas = models.FloatField(blank=True, null=True)
-    lux = models.IntegerField(blank=True, null=True)
-    db = models.FloatField(blank=True, null=True)
-    dt = models.DateTimeField(blank=True, null=True)
-
-    class Meta:
-        # managed = False
-        db_table = 'zeroData'
 
 
 class Feedback(models.Model):
--- smartcampus/core/tests.py
+++ smartcampus/core/tests.py
@@ -7,81 +7,109 @@ from .forms import FeedbackForm
 class LocationModelTest(TestCase):
 
     def test_string_representation(self):
+        """
+        Should return True for location name.
+        """
         location = Location(location_name="Babbio Room 210")
         self.assertEqual(str(location), location.location_name)
 
     def test_verbose_name(self):
+        """
+        Should return True for table name.
+        """
         self.assertEqual(str(Location._meta.db_table), "core_location")
 
 
 class DataModelTest(TestCase):
 
     def test_verbose_name(self):
+        """
+        Should return True for table name.
+        """
         self.assertEqual(str(Data._meta.db_table), "data")
 
 
 class PressureModelTest(TestCase):
 
     def test_verbose_name(self):
+        """
+        Should return True for table name.
+        """
         self.assertEqual(str(Pressure._meta.db_table), "core_pressure")
 
 
 class SensortypeModelTest(TestCase):
 
     def test_verbose_name(self):
+        """
+        Should return True for table name.
+        """
         self.assertEqual(str(Sensortype._meta.db_table), "core_sensortype")
 
 
 class SingleboardcomputerModelTest(TestCase):
 
     def test_verbose_name(self):
+        """
+        Should return True for table name.
+        """
         self.assertEqual(str(Singleboardcomputer._meta.db_table), "core_singleboardcomputer")
 
 
 class SingleboardcomputertypeModelTest(TestCase):
 
     def test_verbose_name(self):
+        """
+        Should return True for table name.
+        """
         self.assertEqual(str(Singleboardcomputertype._meta.db_table), "core_singleboardcomputertype")
 
 
 class TemperatureModelTest(TestCase):
 
     def test_verbose_name(self):
-        self.assertEqual(str(Temperature._meta.db_table), "Temperature")
+        """
+        Should return True for table name.
+        """
+        self.assertEqual(str(Temperature._meta.db_table), "core_temperature")
 
 
 class UserModelTest(TestCase):
 
     def test_verbose_name(self):
+        """
+        Should return True for table name.
+        """
         self.assertEqual(str(User._meta.db_table), "core_user")
-
-
-class ZerodataModelTest(TestCase):
-
-    def test_verbose_name(self):
-        self.assertEqual(str(Zerodata._meta.db_table), "zeroData")
 
 
 # Urls
 class ProjectTests(TestCase):
 
     def test_homepage(self):
-        """ Test the status code for homepage should equal 200"""
+        """
+        Test the status code for homepage should equal 200.
+        """
         response = self.client.get('/')
         self.assertEqual(response.status_code, 200)
 
     def test_locationpage(self):
-        """ Test the status code for location page should equal 200"""
+        """
+        Test the status code for location page should equal 200.
+        """
         response = self.client.get('/select_location')
         self.assertEqual(response.status_code, 200)
 
     def test_campus_reportpage(self):
-        """ Test the status code for campus report page should equal 200"""
+        """
+        Test the status code for campus report page should equal 200.
+        """
         response = self.client.get('/campus_report')
         self.assertEqual(response.status_code, 200)
 
     def test_feedbackpage(self):
-        """ Test the status code for feedback page should equal 200"""
+        """
+        Test the status code for feedback page should equal 200.
+        """
         response = self.client.get('/feedback')
         self.assertEqual(response.status_code, 200)
-
