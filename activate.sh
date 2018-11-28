9eef9df8fe21a2d6f18c096f447844da6d2b5abd
Vincent Porta <vporta7@gmail.com>
2018-11-27 23:47:34 -0500

    delete

--- activate.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-export DATABASE_NAME="readings01"
-export DATABASE_USER="stevens"
-export DATABASE_PASSWORD="Stevens01"
-export DATABASE_HOST="sc01reading.cowctspza6qy.us-west-2.rds.amazonaws.com"
-export DATABASE_PORT=3306
-export SENDGRID_API_KEY="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"
-
